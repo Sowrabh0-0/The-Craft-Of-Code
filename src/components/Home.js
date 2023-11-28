de3d45ed9f69a932789c2cd4e78bb9d1453a4cdf
Sowrabh0-0 <sksseervi@gmail.com>
2023-11-29 01:32:26 +0530

    home done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import styles from '../../assets/styles/Home.module.css'; 
+import styles from '../assets/styles/Home.module.css'; 
 
 const Home = () => {
   return (
     <div className={styles.homeContainer}>
-      <h1>Welcome to the Home Page</h1>
     </div>
   );
 };
