9093321b2a82c336ba8a4a03fa47ef9f38e9c548
HSR2003 <harisankarr2003@gmail.com>
2023-12-06 12:02:04 +0530

    resp

--- src/components/Site3.js
+++ src/components/Site3.js
@@ -5,25 +5,25 @@ const Site3 = () => {
   return (
     <div className={styles.site10} id="3">
       <SideNavbar/>
-      <img
-        className={styles.adithyanarayananshankar4RemoIcon}
-        alt=""
-        src="./images/Site3.png"
-      />
-      <div className={styles.iAmAdithyanarayananContainer}>
-        <p className={styles.iAmAdithyanarayanan}>
-          I am Adithyanarayanan Shankar,
-        </p>
-        <p className={styles.blankLine}>&nbsp;</p>
-        <p className={styles.aSingerVoiceArtistAndMus}>
-          <span>
-            <span className={styles.aSingerVoice}>
-              a singer, voice artist and musician based out of London, the United Kingdom
+        <img
+          className={styles.adithyanarayananshankar4RemoIcon}
+          alt=""
+          src="./images/Site3.png"
+        />
+        <div className={styles.iAmAdithyanarayananContainer}>
+          <p className={styles.iAmAdithyanarayanan}>
+            I am Adithyanarayanan Shankar,
+          </p>
+          <p className={styles.blankLine}>&nbsp;</p>
+          <p className={styles.aSingerVoiceArtistAndMus}>
+            <span>
+              <span className={styles.aSingerVoice}>
+                a singer, voice artist and musician based out of London, the United Kingdom
+              </span>
             </span>
-          </span>
-        </p>
-      </div>
-      <div
+          </p>
+        </div>
+        <div
         className={styles.iWorkAs}
       >{`I work as a Project Manager for a tech giant in the IT field and practice content writing and psychology out of passion. I have more than three years of experience in the field of writing and have managed various types of projects for both SMBs and Corporates. `}</div>
     </div>
